10c1850fd0f6ffc9592a7092f5226dcbc368c304
gergeorg <georgey.gerasimov@gmail.com>
2025-10-21 20:43:24 +0300

    fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
+    define: {
+    __VUE_PROD_DEVTOOLS__: false,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
